3a4572468242299cb099938340cc16fd7d4418e3
Simon Brown <simon@structurizr.com>
2023-02-18 17:37:16 +0000

    Updates structurizr-export dependency, fixes compilation errors from breaking change in transitive structurizr-core dependency.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,9 +6,8 @@ dependencyResolutionManagement {
         create("libs") {
             plugin("spotbugs", "com.github.spotbugs").version("5.0.13")
             plugin("git-versioning", "me.qoomon.git-versioning").version("6.3.6")
-            library("structurizr-export", "com.structurizr", "structurizr-export").version("1.8.2")
-            library("structurizr-core", "com.structurizr", "structurizr-core").version("1.17.0")
-            library("structurizr-client", "com.structurizr", "structurizr-client").version("1.17.0")
+            library("structurizr-export", "com.structurizr", "structurizr-export").version("1.9.0")
+            library("structurizr-client", "com.structurizr", "structurizr-client").version("1.20.1")
             library("junit-jupyter", "org.junit.jupiter", "junit-jupiter").version("5.9.1")
         }
     }
