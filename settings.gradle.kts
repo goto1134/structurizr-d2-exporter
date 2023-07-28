999616a23dd2146646eab77db666329ec444e27f
goto1134 <1134togo@gmail.com>
2023-07-28 22:02:05 +0200

    build: update Structurizr libraries to latest versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,8 @@ dependencyResolutionManagement {
             plugin("git-versioning", "me.qoomon.git-versioning").version("6.3.6")
             plugin("kotlin", "org.jetbrains.kotlin.jvm").version("1.8.20")
             plugin("dokka", "org.jetbrains.dokka").version("1.8.10")
-            library("structurizr-export", "com.structurizr", "structurizr-export").version("1.14.0")
-            library("structurizr-client", "com.structurizr", "structurizr-client").version("1.23.0")
+            library("structurizr-export", "com.structurizr", "structurizr-export").version("1.16.0")
+            library("structurizr-client", "com.structurizr", "structurizr-client").version("1.26.1")
             library("junit-jupyter", "org.junit.jupiter", "junit-jupiter").version("5.9.1")
         }
     }
