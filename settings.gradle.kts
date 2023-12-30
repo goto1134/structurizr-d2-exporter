fb9e9aabc176a9d98211677729dc298e834536fe
goto1134 <1134togo@gmail.com>
2023-12-30 19:13:30 +0100

    build: update dependencies to latest versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,13 @@ include("lib")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            plugin("spotbugs", "com.github.spotbugs").version("5.0.13")
-            plugin("git-versioning", "me.qoomon.git-versioning").version("6.3.6")
-            plugin("kotlin", "org.jetbrains.kotlin.jvm").version("1.8.20")
-            plugin("dokka", "org.jetbrains.dokka").version("1.8.10")
-            library("structurizr-export", "com.structurizr", "structurizr-export").version("1.16.0")
-            library("structurizr-client", "com.structurizr", "structurizr-client").version("1.26.1")
-            library("junit-jupyter", "org.junit.jupiter", "junit-jupiter").version("5.9.1")
+            plugin("spotbugs", "com.github.spotbugs").version("6.0.4")
+            plugin("git-versioning", "me.qoomon.git-versioning").version("6.4.3")
+            plugin("kotlin", "org.jetbrains.kotlin.jvm").version("1.9.22")
+            plugin("dokka", "org.jetbrains.dokka").version("1.9.10")
+            library("structurizr-export", "com.structurizr", "structurizr-export").version("1.19.0")
+            library("structurizr-client", "com.structurizr", "structurizr-client").version("1.29.0")
+            library("junit-jupyter", "org.junit.jupiter", "junit-jupiter").version("5.10.1")
         }
     }
 }
