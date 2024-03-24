af6cf3eb0a60519b24c1ecc9c78470a20c380a4e
goto1134 <1134togo@gmail.com>
2024-03-24 12:38:38 +0100

    build: update dependency versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,13 +4,13 @@ include("lib")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            plugin("spotbugs", "com.github.spotbugs").version("6.0.4")
+            plugin("spotbugs", "com.github.spotbugs").version("6.0.9")
             plugin("git-versioning", "me.qoomon.git-versioning").version("6.4.3")
-            plugin("kotlin", "org.jetbrains.kotlin.jvm").version("1.9.22")
-            plugin("dokka", "org.jetbrains.dokka").version("1.9.10")
-            library("structurizr-export", "com.structurizr", "structurizr-export").version("1.19.0")
-            library("structurizr-client", "com.structurizr", "structurizr-client").version("1.29.0")
-            library("junit-jupyter", "org.junit.jupiter", "junit-jupiter").version("5.10.1")
+            plugin("kotlin", "org.jetbrains.kotlin.jvm").version("1.9.23")
+            plugin("dokka", "org.jetbrains.dokka").version("1.9.20")
+            library("structurizr-export", "com.structurizr", "structurizr-export").version("2.1.1")
+            library("structurizr-client", "com.structurizr", "structurizr-client").version("2.1.1")
+            library("junit-jupyter", "org.junit.jupiter", "junit-jupiter").version("5.10.2")
         }
     }
 }
