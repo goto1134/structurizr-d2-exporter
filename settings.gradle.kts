6b841900744de52ebc6f0d37d1791285eda1103e
goto1134 <1134togo@gmail.com>
2024-12-08 08:21:35 +0100

    chore: update structurizr libraries to version 3.2.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,8 @@ dependencyResolutionManagement {
             plugin("git-versioning", "me.qoomon.git-versioning").version("6.4.3")
             plugin("kotlin", "org.jetbrains.kotlin.jvm").version("1.9.23")
             plugin("dokka", "org.jetbrains.dokka").version("1.9.20")
-            library("structurizr-export", "com.structurizr", "structurizr-export").version("2.1.1")
-            library("structurizr-dsl", "com.structurizr", "structurizr-dsl").version("2.1.1")
+            library("structurizr-export", "com.structurizr", "structurizr-export").version("3.2.0")
+            library("structurizr-dsl", "com.structurizr", "structurizr-dsl").version("3.2.0")
             library("junit-jupyter", "org.junit.jupiter", "junit-jupiter").version("5.10.2")
         }
     }
