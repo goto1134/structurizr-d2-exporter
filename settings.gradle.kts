f429e262820ea38aaa7abd001bc62451c7c730d3
goto1134 <1134togo@gmail.com>
2024-12-08 08:27:07 +0100

    chore: update plugin versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,10 +4,10 @@ include("lib")
 dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
-            plugin("spotbugs", "com.github.spotbugs").version("6.0.9")
-            plugin("git-versioning", "me.qoomon.git-versioning").version("6.4.3")
-            plugin("kotlin", "org.jetbrains.kotlin.jvm").version("1.9.23")
-            plugin("dokka", "org.jetbrains.dokka").version("1.9.20")
+            plugin("spotbugs", "com.github.spotbugs").version("6.0.26")
+            plugin("git-versioning", "me.qoomon.git-versioning").version("6.4.4")
+            plugin("kotlin", "org.jetbrains.kotlin.jvm").version("2.1.0")
+            plugin("dokka", "org.jetbrains.dokka").version("2.0.0-Beta")
             library("structurizr-export", "com.structurizr", "structurizr-export").version("3.2.0")
             library("structurizr-dsl", "com.structurizr", "structurizr-dsl").version("3.2.0")
             library("junit-jupyter", "org.junit.jupiter", "junit-jupiter").version("5.11.3")
