d67e6378f7768f54d31a8370e2cedcfa533e8af8
goto1134 <1134togo@gmail.com>
2024-12-10 19:29:08 +0100

    chore: update structurizr libraries to version 3.2.1
    to resolve flaky tests

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,8 @@ dependencyResolutionManagement {
             plugin("git-versioning", "me.qoomon.git-versioning").version("6.4.4")
             plugin("kotlin", "org.jetbrains.kotlin.jvm").version("2.1.0")
             plugin("dokka", "org.jetbrains.dokka").version("2.0.0-Beta")
-            library("structurizr-export", "com.structurizr", "structurizr-export").version("3.2.0")
-            library("structurizr-dsl", "com.structurizr", "structurizr-dsl").version("3.2.0")
+            library("structurizr-export", "com.structurizr", "structurizr-export").version("3.2.1")
+            library("structurizr-dsl", "com.structurizr", "structurizr-dsl").version("3.2.1")
             library("junit-jupyter", "org.junit.jupiter", "junit-jupiter").version("5.11.3")
         }
     }
